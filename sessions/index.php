4ea8df87f70841024fe3e34cb4a43dd1866a593f
Aidan Smith <originalaidan@gmail.com>
2025-01-27 09:31:50 -0700

    added sessions to index - showing a welcome back msg - basic styling

--- sessions/index.php
+++ sessions/index.php
@@ -1,18 +1,51 @@
+<?php 
+    // Initalizing a session
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sessions</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto;
+        }
+        h1 {
+            color: green;
+        }
+        h2 {
+            color: blueviolet;
+        }
+    </style>
 </head>
 <body>
     <h1>Welcome to my website!</h1>
+    <?php 
+        if(isset($_SESSION['fname'])){
+            // If the session named fname exists...
+            echo "<h2>Welcome back, ".$_SESSION['fname'].".</h2>";
+        }
+        //Else statement opened here
+        else{
+            
+    ?>
     <h2>Please enter your name:</h2>
-    <style></style>
     <form method="post" action="setname.php">
         <label for="name">Your first name:</label>
         <input required placeholder="Enter your first name!" type="text" name="firstName">
         <input type="submit" name="submit" value="Enter">
     </form>
+    <!-- Closing out the else statement from line 39  so we dont have to echo the whole form -->
+    <?php } ?>
 </body>
 </html>
--- sessions/setname.php
+++ sessions/setname.php
@@ -14,15 +14,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sessions | Results</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto;
+        }
+        h1 {
+            color: green;
+        }
+        h2 {
+            color: blueviolet;
+        }
+    </style>
 </head>
 <body>
     <h1>Your profile page</h1>
     <?php
         // If statement to check if session is initalized - and if it has a value
         if(isset($_SESSION['fname'])){
-            echo "<h2>Good morning, ".$_SESSION['fname']."</h2>";
+            echo "<h2>Good morning, ".$_SESSION['fname'].".</h2>";
         }else { // If not, it will display this message on screen!
-            echo "<h2>Please enter a name!</h2>";
+            echo "<h2>You do not have authentication to view this page!</h2>";
         }
 
         // If you want to us a redirect - would be something like this
